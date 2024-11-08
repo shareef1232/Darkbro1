59c70be431a9d2fba97ce61c63a8dcf2be527ff3
shareef1232 <shareeftamadaddi70@gmail.com>
2024-11-08 07:12:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ else:
 INDEX_CHANNELS = [int(index_channels) if index_channels.startswith("-") else index_channels for index_channels in environ.get('INDEX_CHANNELS', '-1002379215158').split()]
 if len(INDEX_CHANNELS) == 0:
     print('Info - INDEX_CHANNELS is empty')
-LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002302145191')
+LOG_CHANNEL = environ.get('LOG_CHANNEL', '-1002018134987')
 if len(LOG_CHANNEL) == 0:
     print('Error - LOG_CHANNEL is missing, exiting now')
     exit()
